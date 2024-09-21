1a28cc20a84fe412ed3715737b03d9409ec9b709
Leesihyeon9 <krsihy@naver.com>
2024-09-21 22:18:02 +0900

    24-09-20quiz
    0920 과제입니다

---
+++ 24년도 컴프 2학기/pythonProject/240920 과제.py
@@ -0,0 +1,23 @@
+# 퀴즈 1
+word = "Hello, World!"
+print(word[0:5])
+
+# 퀴즈 2
+result = (7 + 3) ** 2
+
+# 퀴즈 3
+A = [1, 2, 3, 4, 5]
+result_list = []
+
+result_list.append(A[4])
+result_list.append(A[3])
+result_list.append(A[2])
+result_list.append(A[1])
+result_list.append(A[0])
+
+print(result_list)
+
+# 퀴즈 4
+fruits = {'apple': 3, 'banana': 5, 'cherry': 2}
+banana_value = fruits['banana']
+print(banana_value)
